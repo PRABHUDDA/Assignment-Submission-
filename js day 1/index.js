de6309806db509285a630f91adb998e52a179e44
Prabhudda G B Goudar <79395762+PRABHUDDA@users.noreply.github.com>
2021-08-06 12:19:13 +0530

    Day one assignment submitted

---
+++ js day 1/index.js
@@ -0,0 +1,11 @@
+const a = 20;
+const b = 10;
+
+let add,sub,mul,div,modu;
+add = a + b;
+sub = a - b;
+mul = a * b;
+div = a / b;
+modu = a % b;
+console.log(`add output = ${add} |`,`Sub output = ${sub}  |`,`mul output = ${mul}  |`,`div output = ${div}  |`,`modu output = ${modu} `);
+
